73955795be5576df0ed97fe76766194af0575472
Sam Kington <github@illuminated.co.uk>
2015-02-18 22:05:39 +0000

    Explain how to use this.

--- open-on-mac.pl
+++ open-on-mac.pl
@@ -1,5 +1,8 @@
 #!/usr/bin/env perl
 # Open a URL on my local Mac, not this VM.
+# Set the BROWSER environment variable to this script's path and
+# e.g. /usr/bin/sensible-browser will call it if you use
+# something like Browser::Open to open URLs in a platform-agnostic manner.
 
 use strict;
 use warnings;
